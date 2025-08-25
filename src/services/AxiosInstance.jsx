d9b40903334aa0c86980ee5e04d9b2570a5927ac
Tarun Kumar Behera <tarunkumarbehera8249@gmail.com>
2025-08-25 20:45:08 +0530

    added server url

--- src/services/AxiosInstance.jsx
+++ src/services/AxiosInstance.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = import.meta.env.VITE_BACKEND_URL;
+const baseUrl =import.meta.env.PROD ? "https://forever-backend-3www.onrender.com" : import.meta.env.VITE_BACKEND_URL;
 
 export const axiosInstance = axios.create(
     {
